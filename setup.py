34930d4681983ab09d05f454b98df65690bd8e74
marcel <rieger@physik.rwth-aachen.de>
2014-11-30 16:04:41 +0100

    Initial commit with running code.

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# python imports
+from distutils.core import setup
+
+
+setup(
+    name         = "pymitter",
+    version      = "0.0.1",
+    packages     = ["pymitter"],
+    description  = "Python port of the extended Node.js EventEmitter2 approach.",
+    author       = "Marcel Rieger",
+    author_email = "marcelrieger@icloud.com",
+    url          = "https://github.com/riga/pymitter",
+    keywords     = [
+        "event", "emitter", "eventemitter", "wildcard", "node", "nodejs"
+    ],
+    classifiers  = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
