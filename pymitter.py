51fb1ac7dc1e43a133cb1bd7bf1bd960f1d3010d
Marcel R <github.riga@icloud.com>
2023-08-20 11:55:09 +0200

    Version 0.4.1.

--- pymitter.py
+++ pymitter.py
@@ -11,7 +11,7 @@ __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/pymitter"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = ["EventEmitter", "Listener"]
 
 
