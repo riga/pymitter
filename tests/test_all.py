3e0eed7549aabacdf0ac92f4e277931a3d545668
Marcel R. <github.riga@icloud.com>
2025-07-08 13:49:53 +0200

    Format.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,14 +1,10 @@
-# coding: utf-8
-
-import unittest
-import sys
 import asyncio
+import unittest
 
 from pymitter import EventEmitter
 
 
 class SyncTestCase(unittest.TestCase):
-
     def test_callback_usage(self):
         ee = EventEmitter()
         stack = []
@@ -19,7 +15,7 @@ class SyncTestCase(unittest.TestCase):
         ee.on("callback_usage", handler)
 
         ee.emit("callback_usage", "foo")
-        self.assertTrue(tuple(stack) == ("callback_usage_foo",))
+        assert tuple(stack) == ("callback_usage_foo",)
 
     def test_decorator_usage(self):
         ee = EventEmitter()
@@ -30,7 +26,7 @@ class SyncTestCase(unittest.TestCase):
             stack.append("decorator_usage_" + arg)
 
         ee.emit("decorator_usage", "bar")
-        self.assertTrue(tuple(stack) == ("decorator_usage_bar",))
+        assert tuple(stack) == ("decorator_usage_bar",)
 
     def test_ttl_on(self):
         ee = EventEmitter()
@@ -41,10 +37,10 @@ class SyncTestCase(unittest.TestCase):
             stack.append("ttl_on_" + arg)
 
         ee.emit("ttl_on", "foo")
-        self.assertTrue(tuple(stack) == ("ttl_on_foo",))
+        assert tuple(stack) == ("ttl_on_foo",)
 
         ee.emit("ttl_on", "bar")
-        self.assertTrue(tuple(stack) == ("ttl_on_foo",))
+        assert tuple(stack) == ("ttl_on_foo",)
 
     def test_ttl_once(self):
         ee = EventEmitter()
@@ -55,10 +51,10 @@ class SyncTestCase(unittest.TestCase):
             stack.append("ttl_once_" + arg)
 
         ee.emit("ttl_once", "foo")
-        self.assertTrue(tuple(stack) == ("ttl_once_foo",))
+        assert tuple(stack) == ("ttl_once_foo",)
 
         ee.emit("ttl_once", "bar")
-        self.assertTrue(tuple(stack) == ("ttl_once_foo",))
+        assert tuple(stack) == ("ttl_once_foo",)
 
     def test_walk_nodes(self):
         # helper to recursively convert itertables to tuples
@@ -66,30 +62,21 @@ class SyncTestCase(unittest.TestCase):
             return tuple((obj if isinstance(obj, str) else t(obj)) for obj in iterable)
 
         ee = EventEmitter()
-        self.assertEqual(
-            t(ee._event_tree.walk_nodes()),
-            (),
-        )
+        assert t(ee._event_tree.walk_nodes()) == ()
 
         # normal traversal
         ee.on("foo.bar.test")(lambda: None)
         ee.on("foo.bar.test2")(lambda: None)
-        self.assertEqual(
-            t(ee._event_tree.walk_nodes()),
-            (
-                ("foo", ("foo",), ("bar",)),
-                ("bar", ("foo", "bar"), ("test", "test2")),
-                ("test", ("foo", "bar", "test"), ()),
-                ("test2", ("foo", "bar", "test2"), ()),
-            ),
+        assert t(ee._event_tree.walk_nodes()) == (
+            ("foo", ("foo",), ("bar",)),
+            ("bar", ("foo", "bar"), ("test", "test2")),
+            ("test", ("foo", "bar", "test"), ()),
+            ("test2", ("foo", "bar", "test2"), ()),
         )
 
         # empty tree after removing all listeners
         ee.off_all()
-        self.assertEqual(
-            t(ee._event_tree.walk_nodes()),
-            (),
-        )
+        assert t(ee._event_tree.walk_nodes()) == ()
 
         # traversal with in-place updates
         ee.on("foo.bar.test")(lambda: None)
@@ -99,13 +86,10 @@ class SyncTestCase(unittest.TestCase):
             stack.append(t((name, path, children)))
             if name == "bar":
                 children.remove("test2")
-        self.assertEqual(
-            t(stack),
-            (
-                ("foo", ("foo",), ("bar",)),
-                ("bar", ("foo", "bar"), ("test", "test2")),
-                ("test", ("foo", "bar", "test"), ()),
-            ),
+        assert t(stack) == (
+            ("foo", ("foo",), ("bar",)),
+            ("bar", ("foo", "bar"), ("test", "test2")),
+            ("test", ("foo", "bar", "test"), ()),
         )
 
     def test_on_wildcards(self):
@@ -121,24 +105,24 @@ class SyncTestCase(unittest.TestCase):
             ee.emit(emit)
             return tuple(stack) == (token,)
 
-        self.assertTrue(hits("on_all.*", "on_all.foo"))
-        self.assertTrue(hits("on_all.foo", "on_all.*"))
-        self.assertTrue(hits("on_all.*", "on_all.*"))
+        assert hits("on_all.*", "on_all.foo")
+        assert hits("on_all.foo", "on_all.*")
+        assert hits("on_all.*", "on_all.*")
 
-        self.assertTrue(hits("on_all.foo.bar", "on_all.*.bar"))
-        self.assertTrue(hits("on_all.*.bar", "on_all.foo.bar"))
+        assert hits("on_all.foo.bar", "on_all.*.bar")
+        assert hits("on_all.*.bar", "on_all.foo.bar")
 
-        self.assertTrue(hits("on_all.fo?", "on_all.foo"))
-        self.assertTrue(hits("on_all.foo", "on_all.fo?"))
-        self.assertTrue(hits("on_all.?", "on_all.?"))
+        assert hits("on_all.fo?", "on_all.foo")  # codespell:ignore
+        assert hits("on_all.foo", "on_all.fo?")  # codespell:ignore
+        assert hits("on_all.?", "on_all.?")
 
-        self.assertFalse(hits("on_all.f?", "on_all.foo"))
-        self.assertFalse(hits("on_all.foo", "on_all.f?"))
-        self.assertFalse(hits("on_all.f?", "on_all.?"))
-        self.assertFalse(hits("on_all.?", "on_all.f?"))
+        assert not hits("on_all.f?", "on_all.foo")
+        assert not hits("on_all.foo", "on_all.f?")
+        assert not hits("on_all.f?", "on_all.?")
+        assert not hits("on_all.?", "on_all.f?")
 
-        self.assertFalse(hits("on_all.foo.bar", "on_all.*"))
-        self.assertFalse(hits("on_all.*", "on_all.foo.bar"))
+        assert not hits("on_all.foo.bar", "on_all.*")
+        assert not hits("on_all.*", "on_all.foo.bar")
 
     def test_on_any(self):
         ee = EventEmitter()
@@ -153,7 +137,7 @@ class SyncTestCase(unittest.TestCase):
             stack.append("bar")
 
         ee.emit("foo")
-        self.assertEqual(tuple(stack), ("foo", "bar"))
+        assert tuple(stack) == ("foo", "bar")
 
     def test_off_any(self):
         ee = EventEmitter()
@@ -164,14 +148,14 @@ class SyncTestCase(unittest.TestCase):
             stack.append("foo")
 
         ee.emit("xyz")
-        self.assertEqual(tuple(stack), ("foo",))
+        assert tuple(stack) == ("foo",)
 
         del stack[:]
         ee.off_any(handler1)
 
         ee.emit("xyz")
-        self.assertEqual(tuple(stack), ())
-        self.assertEqual(ee.num_listeners, 0)
+        assert tuple(stack) == ()
+        assert ee.num_listeners == 0
 
     def test_off_all(self):
         ee = EventEmitter()
@@ -184,10 +168,10 @@ class SyncTestCase(unittest.TestCase):
         def handler2():
             pass
 
-        self.assertEqual(ee.num_listeners, 2)
+        assert ee.num_listeners == 2
 
         ee.off_all()
-        self.assertEqual(ee.num_listeners, 0)
+        assert ee.num_listeners == 0
 
     def test_listeners(self):
         ee = EventEmitter(wildcard=True)
@@ -212,11 +196,11 @@ class SyncTestCase(unittest.TestCase):
         def h5():
             pass
 
-        self.assertEqual(tuple(ee.listeners_any()), (h5,))
-        self.assertEqual(tuple(ee.listeners_all()), (h1, h2, h3, h4, h5))
-        self.assertEqual(tuple(ee.listeners("foo")), (h1, h2))
-        self.assertEqual(tuple(ee.listeners("bar")), (h3,))
-        self.assertEqual(tuple(ee.listeners("ba?")), (h3, h4))
+        assert tuple(ee.listeners_any()) == (h5,)
+        assert tuple(ee.listeners_all()) == (h1, h2, h3, h4, h5)
+        assert tuple(ee.listeners("foo")) == (h1, h2)
+        assert tuple(ee.listeners("bar")) == (h3,)
+        assert tuple(ee.listeners("ba?")) == (h3, h4)
 
     def test_emit_all(self):
         ee = EventEmitter(wildcard=True)
@@ -227,7 +211,7 @@ class SyncTestCase(unittest.TestCase):
             stack.append("emit_all.foo")
 
         ee.emit("emit_all.*")
-        self.assertTrue(stack[-1] == "emit_all.foo")
+        assert stack[-1] == "emit_all.foo"
 
     def test_on_reverse_pattern(self):
         ee = EventEmitter(wildcard=True)
@@ -246,11 +230,11 @@ class SyncTestCase(unittest.TestCase):
             stack.append("on_foo_bar_baz_test")
 
         ee.emit("foo.ba?")
-        self.assertTrue(tuple(stack) == ("on_foo_bar", "on_foo_baz"))
+        assert tuple(stack) == ("on_foo_bar", "on_foo_baz")
 
         del stack[:]
         ee.emit("foo.bar.*.test")
-        self.assertTrue(tuple(stack) == ("on_foo_bar_baz_test",))
+        assert tuple(stack) == ("on_foo_bar_baz_test",)
 
     def test_delimiter(self):
         ee = EventEmitter(wildcard=True, delimiter=":")
@@ -261,7 +245,7 @@ class SyncTestCase(unittest.TestCase):
             stack.append("delimiter")
 
         ee.emit("delimiter:foo")
-        self.assertTrue(tuple(stack) == ("delimiter",))
+        assert tuple(stack) == ("delimiter",)
 
     def test_new(self):
         ee = EventEmitter(new_listener=True)
@@ -273,10 +257,11 @@ class SyncTestCase(unittest.TestCase):
 
         def newhandler():
             pass
+
         ee.on("new", newhandler)
         ee.on_any(newhandler)
 
-        self.assertTrue(tuple(stack) == ((newhandler, "new"), (newhandler, None)))
+        assert tuple(stack) == ((newhandler, "new"), (newhandler, None))
 
     def test_max(self):
         ee = EventEmitter(max_listeners=1)
@@ -291,7 +276,7 @@ class SyncTestCase(unittest.TestCase):
             stack.append("max_2")
 
         ee.emit("max")
-        self.assertTrue(tuple(stack) == ("max_1",))
+        assert tuple(stack) == ("max_1",)
 
     def test_tree(self):
         ee = EventEmitter()
@@ -305,109 +290,106 @@ class SyncTestCase(unittest.TestCase):
         def handler2():
             stack.append("max_2")
 
-        self.assertEqual(ee.num_listeners, 2)
-        self.assertEqual(len(ee._event_tree.nodes["max"].listeners), 2)
+        assert ee.num_listeners == 2
+        assert len(ee._event_tree.nodes["max"].listeners) == 2
 
         ee.emit("max")
-        self.assertTrue(tuple(stack) == ("max_1", "max_2"))
+        assert tuple(stack) == ("max_1", "max_2")
         del stack[:]
 
         ee.emit("max")
-        self.assertTrue(tuple(stack) == ("max_1",))
+        assert tuple(stack) == ("max_1",)
         del stack[:]
 
-        self.assertEqual(ee.num_listeners, 1)
-        self.assertTrue("max" in ee._event_tree.nodes)
-        self.assertEqual(len(ee._event_tree.nodes["max"].listeners), 1)
+        assert ee.num_listeners == 1
+        assert "max" in ee._event_tree.nodes
+        assert len(ee._event_tree.nodes["max"].listeners) == 1
 
         ee.off("max", handler1)
-        self.assertEqual(ee.num_listeners, 0)
+        assert ee.num_listeners == 0
 
 
-if sys.version_info[:2] >= (3, 8):
+class AsyncTestCase(unittest.IsolatedAsyncioTestCase):
+    def test_async_callback_usage(self):
+        ee = EventEmitter()
+        stack = []
 
-    class AsyncTestCase(unittest.IsolatedAsyncioTestCase):
+        async def handler(arg):  # noqa: RUF029
+            stack.append("async_callback_usage_" + arg)
 
-        def test_async_callback_usage(self):
-            ee = EventEmitter()
-            stack = []
+        ee.on("async_callback_usage", handler)
 
-            async def handler(arg):
-                stack.append("async_callback_usage_" + arg)
+        ee.emit("async_callback_usage", "foo")
+        assert tuple(stack) == ("async_callback_usage_foo",)
 
-            ee.on("async_callback_usage", handler)
+    def test_async_decorator_usage(self):
+        ee = EventEmitter()
+        stack = []
 
-            ee.emit("async_callback_usage", "foo")
-            self.assertEqual(tuple(stack), ("async_callback_usage_foo",))
+        @ee.on("async_decorator_usage")
+        async def handler(arg):  # noqa: RUF029
+            stack.append("async_decorator_usage_" + arg)
 
-        def test_async_decorator_usage(self):
-            ee = EventEmitter()
-            stack = []
+        ee.emit("async_decorator_usage", "bar")
+        assert tuple(stack) == ("async_decorator_usage_bar",)
 
-            @ee.on("async_decorator_usage")
-            async def handler(arg):
-                stack.append("async_decorator_usage_" + arg)
+    async def test_await_async_callback_usage(self):
+        ee = EventEmitter()
+        stack = []
 
-            ee.emit("async_decorator_usage", "bar")
-            self.assertEqual(tuple(stack), ("async_decorator_usage_bar",))
+        async def handler(arg):  # noqa: RUF029
+            stack.append("await_async_callback_usage_" + arg)
 
-        async def test_await_async_callback_usage(self):
-            ee = EventEmitter()
-            stack = []
+        ee.on("await_async_callback_usage", handler)
 
-            async def handler(arg):
-                stack.append("await_async_callback_usage_" + arg)
+        res = ee.emit_async("await_async_callback_usage", "foo")
+        assert len(stack) == 0
 
-            ee.on("await_async_callback_usage", handler)
+        await res
+        assert tuple(stack) == ("await_async_callback_usage_foo",)
 
-            res = ee.emit_async("await_async_callback_usage", "foo")
-            self.assertEqual(len(stack), 0)
+    async def test_await_async_decorator_usage(self):
+        ee = EventEmitter()
+        stack = []
 
-            await res
-            self.assertEqual(tuple(stack), ("await_async_callback_usage_foo",))
+        @ee.on("await_async_decorator_usage")
+        async def handler(arg):  # noqa: RUF029
+            stack.append("await_async_decorator_usage_" + arg)
 
-        async def test_await_async_decorator_usage(self):
-            ee = EventEmitter()
-            stack = []
+        res = ee.emit_async("await_async_decorator_usage", "bar")
+        assert len(stack) == 0
 
-            @ee.on("await_async_decorator_usage")
-            async def handler(arg):
-                stack.append("await_async_decorator_usage_" + arg)
+        await res
+        assert tuple(stack) == ("await_async_decorator_usage_bar",)
 
-            res = ee.emit_async("await_async_decorator_usage", "bar")
-            self.assertEqual(len(stack), 0)
+    async def test_emit_future(self):
+        ee = EventEmitter()
+        stack = []
 
-            await res
-            self.assertEqual(tuple(stack), ("await_async_decorator_usage_bar",))
+        @ee.on("emit_future")
+        async def handler(arg):  # noqa: RUF029
+            stack.append("emit_future_" + arg)
 
-        async def test_emit_future(self):
-            ee = EventEmitter()
-            stack = []
+        async def test():
+            ee.emit_future("emit_future", "bar")
+            assert len(stack) == 0
 
-            @ee.on("emit_future")
-            async def handler(arg):
-                stack.append("emit_future_" + arg)
+            # let all non-deferred events on the event loop pass
+            await asyncio.sleep(0)
 
-            async def test():
-                ee.emit_future("emit_future", "bar")
-                self.assertEqual(len(stack), 0)
+            assert tuple(stack) == ("emit_future_bar",)
 
-                # let all non-deferred events on the event loop pass
-                await asyncio.sleep(0)
+        await test()
 
-                self.assertEqual(tuple(stack), ("emit_future_bar",))
+    def test_supports_async_callables(self):
+        ee = EventEmitter()
+        stack = []
 
-            await test()
+        class EventHandler:
+            async def __call__(self, arg):
+                stack.append(arg)
 
-        def test_supports_async_callables(self):
-            ee = EventEmitter()
-            stack = []
+        ee.on("event", EventHandler())
 
-            class EventHandler:
-                async def __call__(self, arg):
-                    stack.append(arg)
-
-            ee.on("event", EventHandler())
-
-            ee.emit("event", "arg")
-            self.assertEqual(stack, ["arg"])
+        ee.emit("event", "arg")
+        assert tuple(stack) == ("arg",)
